dec1ac7bb47cfd6ba614b366e0c37f6732683985
whitewalker000 <aalappathak5@gmail.com>
2024-06-28 16:24:35 +0530

    added animation for social icons

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -17,19 +17,19 @@ const Footer = () => {
 
                     <Grid item lg={4} display='flex' justifyContent='center'>
                         <Card className='card' style={{ display: 'flex', justifyContent: 'space-around', padding: '10px' }}>
-                            <div style={{ margin: '0 10px' }}>
-                                <a href='https://www.instagram.com/aalap_pathak/'><FaInstagram size={30} color='white' /></a>
+                            <div className='socials' style={{ margin: '0 10px' }}>
+                                <a href='https://www.instagram.com/aalap_pathak/' target='_blank'><FaInstagram size={30} color='white' /></a>
                             </div>
-                            <div style={{ margin: '0 10px' }}>
+                            <div className='socials' style={{ margin: '0 10px' }}>
                                 <a href='https://github.com/whitewalker000' target='_blank'><FaGithub size={30} color='white' /></a>
                             </div>
-                            <div style={{ margin: '0 10px' }}>
+                            <div className='socials' style={{ margin: '0 10px' }}>
                                 <a href='https://in.linkedin.com/in/aalap-pathak-692638189?original_referer=https%3A%2F%2Fwww.google.com%2F' target='_blank'><FaLinkedin size={30} color='white' /></a>
                             </div>
-                            <div style={{ margin: '0 10px' }}>
+                            <div className='socials' style={{ margin: '0 10px' }}>
                                 <a href='https://aalap-pathak.vercel.app/' target='_blank'><FaGlobe size={30} color='white' /></a>
                             </div>
-                            <div style={{ margin: '0 10px' }}>
+                            <div className='socials' style={{ margin: '0 10px' }}>
                                 <a href='https://wa.me/919913951535' target='_blank'><FaWhatsapp size={30} color='white' /></a>
                             </div>
                         </Card>
